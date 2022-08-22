e404b338a53b27a7bc73070311e03bf24756d0cd
Qzadi <anak0nda@hotmail.com>
2022-08-22 15:13:24 +0300

    Kotiläksy

---
+++ tehtävä 2.4.py
@@ -0,0 +1,13 @@
+luku1 = int(input("anna 1. kokonaisluku"))
+luku2 = int(input("anna 2. kokonaisluku"))
+luku3 = int((input("anna 3. kokonaisluku"))
+
+summa = luku1 + luku2 + luku3
+
+tulo = luku1 * luku2 * luku3
+
+ka Summa / 3
+
+print(f"Summa" = {Summa})
+print(f"Tulo"{Tulo})
+print(f"keskiarvo"{ka})
