d90b14f919be3da35fefd39ae3d1a1174879f344
Qzadi <anak0nda@hotmail.com>
2022-08-22 14:34:34 +0300

    Kotiläksy

--- Tehtävä 2.2.py
+++ Tehtävä 2.2.py
@@ -1,5 +1,5 @@
 import math
-r = float((input)("Anna ympyrän säde: "))
+Säde = float((input)("Anna ympyrän säde: "))
 
-ala = math.pi * math.pow(r,2)
+ala = math.pi * math.pow(Säde,2)
 print(f"Ympyrän pinta-ala on: {ala:.3f}")
