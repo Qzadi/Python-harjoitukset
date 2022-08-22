75ce904b96509c197a26f74311b79edd19bae81a
Qzadi <anak0nda@hotmail.com>
2022-08-23 00:35:45 +0300

    Kotiläksy

---
+++ tehtävä 2.5.py
@@ -0,0 +1,22 @@
+import math
+input1 = float((input) ("Anna leivisköiden määrä"))
+input2 = float((input) ("Anna naulojen määrä"))
+input3 = float((input)("Anna luotien määrä"))
+
+Luoti = 13.3
+Naula = 32 * Luoti
+Leiviskä = 20 * Naula
+
+Tulo = input1 * Leiviskä + input2 * Naula + input3 * Luoti
+
+Kilot= int(Tulo / 1000)
+
+gramma = int(Tulo%1000)
+print("Massa Nykymittojen mukaan:")
+print(Kilot, " Kilogrammaa ja ", end='')
+
+print(gramma, "grammaa")
+
+
+
+
