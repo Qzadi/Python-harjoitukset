a5be6082f423a3badcf60ecc736f7e7499e99be1
Qzadi <anak0nda@hotmail.com>
2022-08-22 15:13:32 +0300

    Kotiläksy

---
+++ tehtävä 2.3.py
@@ -0,0 +1,7 @@
+kanta = float(input("anna suorakulmion kanta :"))
+Korkeus= float(input("anna suorakulmion korkeus :"))
+Ala = kanta * korkeus
+Piiri = (kanta * 2) + (korkeus * 2)
+
+Print (f"Ala" = {A})
+Print (f"Piiri" = {Piiri})
