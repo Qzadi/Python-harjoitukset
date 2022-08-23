1c344230d9db9b5f337136b32622850e18f4acab
Qzadi <anak0nda@hotmail.com>
2022-08-23 09:50:27 +0300

    Kotiläksy

---
+++ Tehtävä 2.6.py
@@ -0,0 +1,9 @@
+import random
+
+#kolminumeroinen luku asteikolla 0-9
+a = [random.randrange(0,9) for i in range(0,3)]
+print (a)
+
+#nelinumeroinen luku asteikolla 1-6
+b = [random.randrange(1,6) for i in range(0,4)]
+print(b)
