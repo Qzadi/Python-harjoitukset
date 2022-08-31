4fc868eef205227d1c7b720d5524b583cd3f4f8b
Qzadi <anak0nda@hotmail.com>
2022-08-31 15:32:52 +0300

    Kotiläksy

--- tehtävä 3.2.py
+++ tehtävä 3.2.py
@@ -2,13 +2,13 @@
 
 hytti = input("Kerro hyttiluokkasi. :")
 
-if hytti == ("LUX" and "lux" and "Lux"):
+if hytti == ("LUX") or hytti == ("Lux") or hytti == ("lux"):
     print("LUX on parvekkeellinen hytti yläkannella.")
-elif hytti == ("A" and "a"):
+elif hytti == "A" or hytti == ("a"):
     print("A on ikkunallinen hytti autokannen yläpuolella.")
-elif hytti == ("B" and "b"):
+elif hytti == ("B") or hytti == ("b"):
     print("B on ikkunaton hytti autokannen yläpuolella.")
-elif hytti == ("C" and "c"):
+elif hytti == ("C") or hytti == ("c"):
     print("C on ikkunaton hytti autokannen alapuolella.")
 else:
     print("Virheellinen Hyttiluokka")
