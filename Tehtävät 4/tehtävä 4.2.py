d3edcc5d3fd3b16624025a2719eaa37678e8714b
Qzadi <anak0nda@hotmail.com>
2022-09-04 22:32:56 +0300

    Kotiläksyt2

---
+++ Tehtävät 4/tehtävä 4.2.py
@@ -0,0 +1,12 @@
+# sentit tuumiksi.
+
+tuumat = int(input("Anna tuumat:"))
+b = tuumat
+c = b * 2.51
+while tuumat != 0:
+    print (f"tuumat sentteinä: {c:6.2f}")
+    tuumat = int(input("Anna tuumat: "))
+    b = tuumat
+    c = b * 2.51
+print ("negatiivinen tuumaluku.")
+
