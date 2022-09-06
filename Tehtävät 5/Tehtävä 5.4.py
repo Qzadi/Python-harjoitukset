fa1a9a9168276579174c8a2176fb8ede9b692ebf
Qzadi <anak0nda@hotmail.com>
2022-09-06 15:47:41 +0300

    Kotiläksyt2

--- Tehtävät 5/Tehtävä 5.4.py
+++ Tehtävät 5/Tehtävä 5.4.py
@@ -1,7 +1,11 @@
 # Kirjoita ohjelma, joka kysyy käyttäjältä viiden kaupungin nimet yksi kerrallaan
 # (käytä for-toistorakennetta nimien kysymiseen) ja tallentaa ne listarakenteeseen.
 # Lopuksi ohjelma tulostaa kaupunkien nimet yksi kerrallaan allekkain samassa järjestyksessä
-# kuin ne syötettiin. käytä for-toistorakennetta nimien kysymiseen ja
-# for/in toistorakennetta niiden läpikäymiseen.
-
-a = input("Anna kaupunki: ")
+# kuin ne syötettiin. käytä for-toistorakennetta nimien kysymiseen ja for/in
+# toistorakennetta niiden läpikäymiseen.
+b = []
+for n in range (5):
+  a = input("Anna kaupunki")
+  b.append(a)
+for n in b:
+    print(n)
